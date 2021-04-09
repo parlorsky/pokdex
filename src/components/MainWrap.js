214efa5a42660c1e2265ebd70694dd7a61635440
parlorsky <tffriend015@gmail.com>
2021-04-09 15:59:18 +0500

    updt1

--- src/components/MainWrap.js
+++ src/components/MainWrap.js
@@ -1,5 +1,5 @@
 import React from "react";
-import s from "./MainWrap.module.scss";
+import s from "./MainWrap.module.css";
 import { Link } from "react-router-dom";
 
 const MainWrap = (props) => {
